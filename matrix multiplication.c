90bf52a28289e7a1bb3ebd76bb16a24f8d1eaeef
DDarkeron <31534102+DDarkeron@users.noreply.github.com>
2017-11-27 17:04:44 +0300

    Update matrix multiplication.c

--- matrix multiplication.c
+++ matrix multiplication.c
@@ -4,12 +4,14 @@ int main(){
     
 
 
-int m=3+rand()%7,n=4+rand()&6,i=0,j=0;
+int m=3+rand()%7,n=4+rand()&6,i=0,j=0,min=101;
 int a[i][j];
+int b[k][l];
     
     for(i<m:i++){
         for(j<n:j++){
             a[i][j]=100-rand()%200;
+            a[i][j]=b[k][l];
             if(a[i][j]<min){
                 mm=i;
                 nn=j;
@@ -24,5 +26,11 @@ int a[i][j];
             printf("%d\t",a[i][j]);
         }
         printf("\n");
+    }
+    i=0;
+    j=0;
+    for(j<=l,j++){
+        a[i][j]=b[k][l-j];
+        a[k][j]=b[i][]
     }
 }
