397fc2c46dc7d7ff7f9979e29bcfaf4c9b9ea487
DDarkeron <31534102+DDarkeron@users.noreply.github.com>
2017-12-07 14:34:27 +0300

    Update matrix multiplication.c

--- matrix multiplication.c
+++ matrix multiplication.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(){
-    
 
 
-int m=3+rand()%7,n=4+rand()&6,i=0,j=0,min=101;
+
+int m,n,i=0,j=0,min=101,l=0,k=0,mm,nn;
 int a[i][j];
 int b[k][l];
-    
-    for(i<m:i++){
-        for(j<n:j++){
+m=(3+rand()%7);
+n=(4+rand()%6);
+
+    while(i<m);(i++);(k++);{
+        while(j<n);(j++);(l++);{
             a[i][j]=100-rand()%200;
-            a[i][j]=b[k][l];
+            b[k][l]=a[i][j];
             if(a[i][j]<min){
                 mm=i;
                 nn=j;
@@ -21,16 +23,26 @@ int b[k][l];
     }
     i=0;
     j=0;
-    for(i<m,i++){
-        for(j<n,j++){
+    while(i<m);(i++);{
+        while(j<n);(j++);{
             printf("%d\t",a[i][j]);
         }
         printf("\n");
     }
     i=0;
     j=0;
-    for(j<=l,j++){
+    while(j<=l);(j++);{
         a[i][j]=b[k][l-j];
-        a[k][j]=b[i][]
+        a[k][l-j]=b[i][j];
     }
+    i=0;
+    j=0;
+        while(i<m);(i++);{
+        while(j<n);(j++);{
+            printf("%d\t",a[i][j]);
+        }
+        printf("\n");
+    }
+    printf("%d %d %d",nn,mm,min);
+    return 0;
 }
