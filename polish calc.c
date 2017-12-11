dfaf40ed3efd1d664552c0f072c7e0ee83fc082c
DDarkeron <31534102+DDarkeron@users.noreply.github.com>
2017-12-11 16:39:47 +0300

    Update polish calc.c

--- polish calc.c
+++ polish calc.c
@@ -1,83 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
-int i=0;
-float array[100];
-void push(float a) //puts number into stack
+#include <string.h>
+int count=0;
+float stek[100];
+void push(float a) /*puts nuber into stack*/
 {
-    array[i]=a;
-    i++;
+    stek[count]=a;
+    count++;
 }
-float pop() //extracts number from stack
+float pop() /*delete number int stack*/
 {
-    float c=array[--i];
-    array[i]='\0';
+    float c=stek[--count];
+    stek[count]='\0';
     return c;
 
 }
-int main()
+int main(int argc,char* argv[])
 {
-    float a,pop2,pop1;
-    int k=1,drob=0;
-    char simvol;
-    while ((a = getchar()) != '\n') //entering number
-        {
-         if (a=='-')
-            k=-1;
-         if (a>= '0' && a <= '9' )
-         {
-            pop2=k*(a-'0');
-            push(pop2); //puts number into stack
-            k=1;
-            if (drob==1)
+    float pop2;
+    int i,drob=0;
+    printf("polish entry \nmultiplication sing = '*'\n");
+    if (argc<=1)
+        printf("u haven't entered anything or entered invalid type of integer");
+    else
+    {
+         for (i=0;i<argc;i++)
+            if (strcmp(argv[i], "+")==0) /*sum*/
             {
-            pop1=pop()/10;
-            pop2=pop();
-            if (pop2<0)
-                pop2=pop1-pop2;
-
-            else
-                pop2=pop2+pop1;
-            drob=0;
-            push(pop2);
+                pop2=pop();
+                push(pop()+pop2);
             }
-         }
-         if (a==','||a=='.')
-            drob=1;
-         if (a==' ')
-         {
-            pop1=pop();
-            pop2=pop()*10;
-            if (pop2<0)
-                pop2=pop2-pop1;
             else
-                pop2=pop2+pop1;
-            push(pop2);
-         }
-         if ((a=='+'||a=='-'||a=='*'||a=='/')&& count>=2)
-            {
-                simvol=a;
-                switch(simvol)
+                if (strcmp(argv[i], "-")==0) /*difference*/
+                {
+                    pop2=pop();
+                    push(pop()-pop2);
+                }
+                else
+                    if (strcmp(argv[i],"'*'")==0) /*multiplication*/
                     {
-                        case '+': pop2=pop();
-                               // printf("\n%f\n",pop2);
-                                  push(pop()+pop2);
-                                  break;
-                        case '-': pop2=pop();
-                                  push(pop()-pop2);
-                                  break;
-                        case '*': pop2=pop();
-                                  push(pop()*pop2);
-                                  break;
-                        case '/': pop2=pop();
-                                  if (pop2!=0.0)
-                                  push(pop() / pop2);
-                                  else printf("na nol' delit' nel'zya");
-                                  break;
+                        pop2=pop();
+                        push(pop()*pop2);
                     }
-
-            }
-        }
-    printf("%.2f",pop());//shows last number
+                    else
+                        if (strcmp(argv[i], "/")==0) /*division*/
+                        {
+                            pop2=pop();
+                            if (pop2!=0.0)
+                                push(pop() / pop2);
+                            else
+                            drob=-1;
+                        }
+                        else
+                            push(atof(argv[i])); /*number*/
+    if (count==0)
+        printf("u haven't entered anything or entered invalid type of integer");
+    else
+        if (drob==-1)
+         printf("u can not divide it by zero");
+        else
+            printf("%.2f",pop());/*types number*/
+    }
     return 0;
 
 }
