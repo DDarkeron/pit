2025075bfaec441ff6576c27330fdff9e1a53e5c
DDarkeron <31534102+DDarkeron@users.noreply.github.com>
2017-12-18 02:51:23 +0300

    Update polish calc.c

--- polish calc.c
+++ polish calc.c
@@ -5,62 +5,62 @@ int count=0;
 float stek[100];
 void push(float a) /*puts nuber into stack*/
 {
-    stek[count]=a;
-    count++;
+stek[count]=a;
+count++;
 }
 float pop() /*delete number int stack*/
 {
-    float c=stek[--count];
-    stek[count]='\0';
-    return c;
+float c=stek[--count];
+stek[count]='\0';
+return c;
 
 }
 int main(int argc,char* argv[])
 {
-    float pop2;
-    int i,drob=0;
-    printf("polish entry \nmultiplication sing = '*'\n");
-    if (argc<=1)
-        printf("u haven't entered anything or entered invalid type of integer");
-    else
-    {
-         for (i=0;i<argc;i++)
-            if (strcmp(argv[i], "+")==0) /*sum*/
-            {
-                pop2=pop();
-                push(pop()+pop2);
-            }
-            else
-                if (strcmp(argv[i], "-")==0) /*difference*/
-                {
-                    pop2=pop();
-                    push(pop()-pop2);
-                }
-                else
-                    if (strcmp(argv[i],"'*'")==0) /*multiplication*/
-                    {
-                        pop2=pop();
-                        push(pop()*pop2);
-                    }
-                    else
-                        if (strcmp(argv[i], "/")==0) /*division*/
-                        {
-                            pop2=pop();
-                            if (pop2!=0.0)
-                                push(pop() / pop2);
-                            else
-                            drob=-1;
-                        }
-                        else
-                            push(atof(argv[i])); /*number*/
-    if (count==0)
-        printf("u haven't entered anything or entered invalid type of integer");
-    else
-        if (drob==-1)
-         printf("u can not divide it by zero");
-        else
-            printf("%.2f",pop());/*types number*/
-    }
-    return 0;
+float pop2;
+int i,drob=0;
+printf("polish entry \nmultiplication sing = '*'\n");
+if (argc<=1)
+printf("u haven't entered anything or entered invalid type of integer");
+else
+{
+for (i=0;i<argc;i++)
+if (strcmp(argv[i], "+")==0) /*sum*/
+{
+ pop2=pop();
+push(pop()+pop2);
+}
+else
+if (strcmp(argv[i], "-")==0) /*difference*/
+{
+pop2=pop();
+push(pop()-pop2);
+}
+else
+if (strcmp(argv[i],"'*'")==0) /*multiplication*/
+{
+pop2=pop();
+push(pop()*pop2);
+}
+else
+if (strcmp(argv[i], "/")==0) /*division*/
+{
+pop2=pop();
+if (pop2!=0.0)
+push(pop() / pop2);
+else
+drob=-1;
+}
+ else
+push(atof(argv[i])); /*number*/
+if (count==0)
+printf("u haven't entered anything or entered invalid type of integer");
+else
+if (drob==-1)
+printf("u can not divide it by zero");
+else
+printf("%.2f",pop());/*types number*/
+}
+return 0;
 
 }
