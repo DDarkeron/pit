a3a2895efd7cbfd4eb25108fcdb648c0766c2c87
DDarkeron <31534102+DDarkeron@users.noreply.github.com>
2017-12-21 19:03:59 +0300

    Update shell.c

--- shell.c
+++ shell.c
@@ -14,7 +14,7 @@ int lsh_cd(char **args);
 int lsh_help(char **args);
 int lsh_exit(char **args);
 
-char *builtin_str[] =
+char *builtin_str[] =   /*Transfer next commands*/
 {
 "cd",
 "help",
@@ -23,9 +23,9 @@ char *builtin_str[] =
 
 int (*builtin_func[]) (char **) =
 {
-&lsh_cd,
-&lsh_help,
-&lsh_exit
+  &lsh_cd,
+  &lsh_help,
+  &lsh_exit
 };
 
 int lsh_num_builtins()
@@ -35,18 +35,19 @@ return sizeof(builtin_str) / sizeof(char *);
 
 int lsh_cd(char **args)
 {
-if (args[1] == NULL) {
-fprintf(stderr, "lsh: expected argument to \"cd\"\n");
+if (args[1] == NULL)
+{
+ fprintf(stderr, "lsh: expected argument to \"cd\"\n");
 } else {
 if (chdir(args[1]) != 0)
 {
-perror("lsh");
+  perror("lsh");
 }
 }
 return 1;
 }
 
-int lsh_help(char **args)
+int lsh_help(char **args) /*Shows all possible commands*/
 {
 int i;
 
@@ -57,7 +58,7 @@ printf(" %s\n", builtin_str[i]);
 return 1;
 }
 
-int lsh_exit(char **args)
+int lsh_exit(char **args) /*Turns off shell*/
 {
 return 0;
 }
@@ -65,15 +66,15 @@ return 0;
 
 char *lsh_read_line(void)
 {
-int bufsize = LSH_RL_BUFSIZE;
-int position = 0;
-char *buffer = malloc(sizeof(char) * bufsize);
-int c;
+  int bufsize = LSH_RL_BUFSIZE;
+  int position = 0;
+  char *buffer = malloc(sizeof(char) * bufsize);
+  int c;
 
 if (!buffer)
 {
-fprintf(stderr, "lsh: allocation error \n");
-exit(EXIT_FAILURE);
+  fprintf(stderr, "lsh: allocation error \n");
+  exit(EXIT_FAILURE);
 }
 
 while (1) {
@@ -82,39 +83,41 @@ c = getchar();
 
 if (c == EOF || c == '\n')
 {
-buffer[position] = '\0';
+  buffer[position] = '\0';
 return buffer;
 } else {
-buffer[position] = c;
+  buffer[position] = c;
 }
-position++;
+  position++;
 
 if (position >= bufsize)
 {
-bufsize += LSH_RL_BUFSIZE;
-buffer = realloc(buffer, bufsize);
-if (!buffer) {
-fprintf(stderr, "lsh: allocation error \n");
-exit(EXIT_FAILURE);
+  bufsize += LSH_RL_BUFSIZE;
+  buffer = realloc(buffer, bufsize);
+if (!buffer) 
+{
+  fprintf(stderr, "lsh: allocation error \n");
+  exit(EXIT_FAILURE);
 }
 }
 }
 }
 char **lsh_split_line(char *line)
 {
-int bufsize = LSH_TOK_BUFSIZE, position = 0;
-char **tokens = malloc(bufsize * sizeof(char*));
-char *token;
+  int bufsize = LSH_TOK_BUFSIZE, position = 0;
+  char **tokens = malloc(bufsize * sizeof(char*));
+  char *token;
 
 if (!tokens) {
-fprintf(stderr, "lsh: allocation error \n");
-exit(EXIT_FAILURE);
+  fprintf(stderr, "lsh: allocation error \n");
+  exit(EXIT_FAILURE);
 }
 
-token = strtok(line, LSH_TOK_DELIM);
-while (token != NULL) {
-tokens[position] = token;
-position++;
+  token = strtok(line, LSH_TOK_DELIM);
+  while (token != NULL)
+{
+  tokens[position] = token;
+  position++;
 
 if (position >= bufsize)
 {
@@ -132,7 +135,7 @@ token = strtok(NULL, LSH_TOK_DELIM);
 tokens[position] = NULL;
 return tokens;
 }
-int lsh_launch(char **args)
+int lsh_launch(char **args) /*Launching the process*/
 {
 pid_t pid, wpid;
 int status;
@@ -182,7 +185,7 @@ char *line;
 char **args;
 int status;
 
-do
+do /* Does main func while command isn't exit */
 {
 printf("> ");
 line = lsh_read_line();
